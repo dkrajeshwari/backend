103d936fc2aecc91b6f5058bc2cb3358d47a2558
D k Rajeshwari <53164760+dkrajeshwari@users.noreply.github.com>
2020-02-11 14:49:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,143 @@
+from flask import Flask, jsonify,request
+from flask_cors import CORS, cross_origin
+import statement1dbope as db
+from flask_pymongo import PyMongo
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity, get_jwt_claims
+
+)
+app = Flask(__name__)
+CORS(app)
+
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/dhi_analytics"
+
+
+mongo = PyMongo(app)
+# Setup the Flask-JWT-Extended extension
+
+app.config['JWT_SECRET_KEY'] = 'super-secret' 
+jwt = JWTManager(app)
+
+
+class UserObject:
+    def __init__(self, username, roles,emlpoyeeGivenId,usn):
+        self.username = username
+        self.roles = roles
+        self.emlpoyeeGivenId = emlpoyeeGivenId
+        self.usn = usn
+ 
+
+@jwt.user_claims_loader
+def add_claims_to_access_token(user):
+    return {'roles': user.roles,"emlpoyeeGivenId":user.emlpoyeeGivenId,"usn":user.usn}
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.username
+
+# Provide a method to create access tokens. The create_access_token()
+# function is used to actually generate the token, and you can return
+# it to the caller however you choose.
+@app.route('/login', methods=['POST'])
+def login():
+    emlpoyeeGivenId = ''
+    usn = ''
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    username = request.json.get('username', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    user = mongo.db.dhi_user.find_one({'email': username})
+    if not user:
+        return jsonify({"msg": "Bad username or password"}), 401
+    roles = [ x['roleName'] for x in user['roles']]
+    if 'employeeGivenId' in user:
+        emlpoyeeGivenId = user["employeeGivenId"]
+    if 'usn' in user:
+        usn = user["usn"]
+    user = UserObject(username=user["email"], roles=roles,emlpoyeeGivenId = emlpoyeeGivenId,usn = usn)
+    # Identity can be any data that is json serializable
+    access_token = create_access_token(identity=user,expires_delta=False)
+    return jsonify(access_token=access_token), 200
+
+@app.route('/message')
+def message():
+    return {"message":"Check you luck"}
+
+
+
+# Protect a view with jwt_required, which requires a valid access token
+# in the request to access.
+
+
+@app.route('/user', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    ret = {
+            'user': get_jwt_identity(),  
+            'roles': get_jwt_claims()['roles'] ,
+            'employeeGivenId':get_jwt_claims()['emlpoyeeGivenId'],
+            'usn':get_jwt_claims()['usn']
+          }
+        
+    return jsonify(ret), 200
+
+
+@app.route('/academicyear')
+def getacademicYear():
+    year = db.getacademicYear()
+    return jsonify({'acdemicYear':year})
+
+@app.route('/termNumber')
+def get_term_numbers():
+    terms = db.get_term_numbers()
+    return jsonify({'term_numbers':terms})
+
+  
+@app.route("/get_user_dept/<email>")
+def get_user_email(email):
+    res = db.get_user_dept(email)
+    return jsonify({"res":res})
+
+@app.route("/get_placement_offer/<term>/<usn>")
+def get_placement_offer(term,usn):
+    res = db.get_placment_offers(term,usn)
+    return jsonify({"res":res})
+
+@app.route("/get_uescore/<year>/<dept>/<usn>/<term>")
+def get_uescore(year,dept,usn,term):
+    res = db.get_uescore(year,dept,usn,term)
+    return jsonify({"res":res})
+
+@app.route("/get_dept_faculty/<empid>")
+def get_dept_faculty(empid):
+    res = db.get_faculties_by_dept(empid)
+    return jsonify({"res":res})
+
+@app.route("/get_faculty_stud_ue/<empid>/<year>/<term>")
+def get_faculty_stud_ue(empid,year,term):
+    fid = db.get_faculty_id(empid)
+    res = db.get_faculty_stu_ue(empid,year,term,fid)
+    return jsonify({"res":res})
+
+@app.route('/get_facul_stud_placement/<empid>/<sem>/<sub>')
+def getSubPlacement(empid,sem,sub):
+    details = db.get_emp_sub_placement(empid,sub,sem)
+    return jsonify({
+        "courseCode":sub,
+        "totalStudents" : details[0],
+        "placedStudents": details[1],
+        "totalPositions": details[2]
+    })
+    
+@app.route('/depts')
+def getAllDept():
+    depts = db.get_all_depts()
+    return jsonify({"depts":depts})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ statement1dbope.py
@@ -0,0 +1,202 @@
+from pymongo import MongoClient
+import pymongo
+from pprint import pprint
+import re
+import bson
+
+db = MongoClient()
+mydb = db.dhi_analytics
+dhi_internal = mydb['dhi_internal']
+dhi_term_details = mydb['dhi_term_detail']
+dhi_student_attendance = mydb['dhi_student_attendance']
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+db = myclient["dhi_analytics"]
+def getacademicYear():
+    academicYear = dhi_internal.aggregate([{"$group":{"_id":"null",
+    "academicYear":{"$addToSet":"$academicYear"}}},{"$project":{"academicYear":"$academicYear","_id":0}}])
+    for year in academicYear:
+        year = year['academicYear']
+    return year
+
+def get_term_numbers():
+    terms_numbers = dhi_term_details.aggregate([ 
+        { "$unwind":"$academicCalendar"}, 
+        {"$group":{"_id":"null","termNumber":{"$addToSet":"$academicCalendar.termNumber"}}},
+        {"$project":{"_id":0}}
+    ])
+    for term in terms_numbers:
+        terms = term['termNumber']
+    terms.sort()
+    return terms
+
+
+def get_user_dept(email):
+    collection = db.dhi_user
+    usn = collection.aggregate([
+        {"$match":{"email":email}},
+        {"$project":{"_id":0,"usn":1}}
+        ])
+    res = []
+    for x in usn:
+        res = x["usn"]
+    res = res[5:7]
+    return res
+
+def get_placment_offers(term, usn):
+
+    collection=db.pms_placement_student_details
+    offers = collection.aggregate([
+        {"$unwind":"$studentList"},
+        {"$match":{"studentList.regNo":usn,"academicYear":term}},
+        {"$project":{"companyName":1,"salary":1,"_id":0}}
+    ])
+    res = []
+    for x in offers:
+        res.append(x)
+    return res
+
+def get_uescore(year,dept,usn,term):
+
+    collection = db.pms_university_exam
+
+    ueScore = collection.aggregate([
+    {"$match":{"academicYear" : year,"deptId" : dept}},
+    {"$unwind":"$terms"},
+    {"$match":{"terms.termNumber":term}},
+    {"$unwind":"$terms.scores"},
+    {"$match":{"terms.scores.usn":usn}},
+    {"$project":{"course":"$terms.scores.courseScores"}},
+    {"$unwind":"$course"},
+    {"$project":{"courseName":"$course.courseName","ueScore":"$course.ueScore","maxUeScore":"$course.maxUeScore","totalScore":"$course.totalScore","_id":0}}
+    ])
+    res = []
+    for x in ueScore:
+        res.append(x)
+    return res
+    
+#returns the list of all faculties in a department
+def get_faculties_by_dept(dept):
+    collection = db.dhi_user
+    pattern = re.compile(f'^{dept}')
+    regex = bson.regex.Regex.from_native(pattern)
+    regex.flags ^= re.UNICODE 
+    faculties = collection.aggregate([
+        {"$match":{"roles.roleName":"FACULTY","employeeGivenId":{"$regex":regex}}},
+        {"$sort":{"name":1}},
+        {"$project":{"employeeGivenId":1,"name":1,"_id":0}}
+    ])
+    res = []    
+    for x in faculties:
+        res.append(x)
+    return res
+
+# get faculty wise student UE score
+def get_faculty_id(empID):
+    collection =db.dhi_internal
+    emp = collection.aggregate([
+    {"$unwind":{"path":"$faculties"}},
+    {"$match":{"faculties.facultyGivenId":empID}},
+    {"$group":{"_id":"null","id":{"$addToSet":"$faculties.facultyId"}}},
+    {"$unwind":{"path":"$id"}},
+    {"$project":{"id":1,"_id":0}}
+    ])
+    res = []
+    for x in emp:
+        res.append(x)
+    for i in res:
+        id = i
+    fac_id = id['id']
+    return fac_id
+
+
+def get_faculty_stu_ue(empid,year,term,id):
+    collection =db.dhi_student_attendance
+    emp = collection.aggregate([
+    {"$match":{"academicYear":year,"students.termNumber":term}},
+    {"$unwind":{'path':"$faculties"}},
+    {"$match":{"faculties.facultyId":id}},
+    {
+    "$lookup":
+    {
+    "from":"pms_university_exam",
+    "localField":"students.usn",
+    "foreignField":"terms.scores.usn",
+    "as":"usn"
+    }
+    },
+    {"$unwind":{'path':"$usn"}},
+    {"$unwind":{'path':"$usn.terms"}},
+    {"$unwind":{'path':"$usn.terms.scores"}},
+    {"$unwind":{'path':"$usn.terms.scores.courseScores"}},
+    {"$match":{"$expr":{"$eq":["$usn.terms.scores.courseScores.courseCode","$courseCode"]}}},
+    {"$group":{"_id":{"course":"$courseName"},"ue":{"$push":"$usn.terms.scores.courseScores.ueScore"}}},
+    {"$project":{"course":"$_id.course","avg":{"$avg":"$ue"},"ue":"$ue","_id":0}}
+    ])
+    res = []
+    for mark in emp:
+        place = get_emp_sub_placement(empid,mark['course'],term)
+        if mark['avg'] != 0:
+            mark['uePercentage'] =  mark['avg']
+        else:
+            mark['uePercentage'] = 0
+        if place[0] != 0:
+            mark['placePercentage'] = 100 * place[1] / place[0]
+        else:
+            mark['placePercentage'] = 0
+        res.append(mark)
+    return res
+
+#placement details of a class handled by empID
+def get_emp_sub_placement(empID,sub,sem):
+    collection = db.dhi_student_attendance
+    students = collection.aggregate([
+        {"$match":{"faculties.employeeGivenId" : empID,"departments.termNumber":sem,"courseName":sub}},
+        {"$unwind":"$students"},
+        {"$group":{"_id":"$courseName","studentUSNs":{"$addToSet":"$students.usn"}}},
+    ])
+    res = []
+    for x in students:
+        res.append(x)
+    print(students)
+    totalStudents = 0
+    filtered = []
+    for x in res:
+        for usn in x["studentUSNs"]:
+            status = get_placed_details(usn)
+            if status!=0:
+                filtered.append(status)
+            totalStudents = len(x["studentUSNs"])
+    return (totalStudents,len(filtered),sum(filtered))
+
+#returns no of placement offers obtained by a student of passed usn
+def get_placed_details(usn):
+    collection = db.pms_placement_student_details
+    people = collection.aggregate([
+    {"$match":{"studentList.regNo":usn}},
+    {"$unwind":"$studentList"},
+    {"$match":{"studentList.regNo":usn}},
+    ])
+    res = []
+    for x in people:
+        res.append(x)
+    return len(res)
+
+#returns the list of all department
+def get_all_depts():
+    collection = db.dhi_user
+    depts = collection.aggregate([
+        {"$match":{"roles.roleName":"FACULTY"}},
+        {"$project":{"_id":0,"employeeGivenId":1}}
+    ])
+    res = []
+    for d in depts:
+        if "employeeGivenId" in d:
+            res.append(d["employeeGivenId"])
+    dept = []
+    for d in res:
+        name = re.findall('([a-zA-Z]*).*',d)
+        if name[0].upper() not in dept:
+            dept.append(name[0].upper())
+    dept.remove('ADM')
+    dept.remove('EC')
+    return dept
